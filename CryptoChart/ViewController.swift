42055820ff7f9907096eef077561112f26dd86b3
Noam Efergan <noamifergan@gmail.com>
2020-06-30 18:27:20 +0300

    Initial Commit

---
+++ CryptoChart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CryptoChart
+//
+//  Created by Noam Efergan on 30/06/2020.
+//  Copyright © 2020 Noam Efergan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
